8c9086bfc193ebb496c470bc208e996a1d2811da
Evan Quirk <evan_quirk@me.com>
2021-02-23 16:54:50 -0800

    feat: add jquery logic for character counter.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-console.log("the DOM is ready");
+  $("#tweet-text").on("input", function() {
+    let charactersLimit = 140 - this.value.length;
+    let counter = $(this).parent().find(".counter");
+    counter.text(charactersLimit);
+    if (charactersLimit < 0) {
+      counter.css("color", "red");
+    } else {
+      counter.css("color", "inherit");
+    }
+  })
 });
