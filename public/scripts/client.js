dcc45abc73bd4ea50a537a919c75c7dc90cc5839
Evan Quirk <evan_quirk@me.com>
2021-02-24 18:09:28 -0800

    refactor: clean up code and remove hard code user data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,3 @@
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
 const formSubmitHandler = function() {
 
   $("form").on("submit", function(event) {
@@ -45,8 +20,6 @@ const formSubmitHandler = function() {
         $("form").trigger("reset");
         $("#tweet-container").empty();
         loadTweets();
-        // 1. reset counter, 2. empty form, 3. empty tweet container . remove . empty >> reload tweets
-        console.log('Success!')
       })
      }
   });
