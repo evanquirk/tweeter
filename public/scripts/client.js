bf85a3c45e9f035b0873318fd2c1e37a231401fa
Evan Quirk <evan_quirk@me.com>
2021-02-24 19:34:15 -0800

    refactor: lint and clean up code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,5 @@
-const formSubmitHandler = function() {
-  $("form").on("submit", function(event) {
+const formSubmitHandler = function () {
+  $("form").on("submit", function (event) {
     event.preventDefault();
     console.log("Button clicked, performing AJAX call...");
     const text = $("#tweet-text").val();
@@ -8,51 +8,49 @@ const formSubmitHandler = function() {
     } else if (text.length > 140) {
       window.alert("Your tweet is too long!");
     } else {
-    $.ajax("/tweets", {
-      method: "POST",
-      data: $("#tweet-text").serialize(),
-      dataType: "text"
-
-    })
-      .then(function () {
-        $(".counter").text(140);
-        $("form").trigger("reset");
-        $("#tweet-container").empty();
-        loadTweets();
+      $.ajax("/tweets", {
+        method: "POST",
+        data: $("#tweet-text").serialize(),
+        dataType: "text"
       })
-     }
+        .then(function () {
+          $(".counter").text(140);
+          $("form").trigger("reset");
+          $("#tweet-container").empty();
+          loadTweets();
+        })
+    }
   });
-  
+
 }
 
-function loadTweets () {
-  $.get("/tweets", function(data) {
+function loadTweets() {
+  $.get("/tweets", function (data) {
     renderTweets(data);
-  }).fail(function(error) {
+  }).fail(function (error) {
     console.log(error);
   });
 }
 
-// loops through tweets (below)
-// calls createTweetElement for each tweet
-// takes return value and appends it to the tweets container
-const renderTweets = function(tweets) {
-  tweets.forEach(function(element) {
+
+const renderTweets = function (tweets) {
+  tweets.forEach(function (element) {
     const $tweet = createTweetElement(element);
     $("#tweet-container").prepend($tweet);
   });
-  
+  // loops through tweets (below)
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
 }
 
-const escape =  function(str) {
+
+const escape = function (str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
 
-const createTweetElement = function(tweet) {
-
-
+const createTweetElement = function (tweet) {
   const $tweet = $(`
   <article>
     <header>
@@ -62,21 +60,24 @@ const createTweetElement = function(tweet) {
       </span>
       <span class="user-handle">${tweet.user.handle}</span>
     </header>
+
     <div class="tweet-body">
-    <p>${escape(tweet.content.text)}</p>
+      <p>${escape(tweet.content.text)}</p>
     </div>
-  <footer>
-    <span>${tweet.created_at}</span>
-    <span class="icons">
-      <i class="fa fa-flag" aria-hidden="true"></i> &nbsp;
-      <i class="fa fa-retweet" aria-hidden="true"></i> &nbsp;
-      <i class="fa fa-heart" aria-hidden="true"></i> &nbsp;
-  </span>
-  </footer>
+
+    <footer>
+      <span>${tweet.created_at}</span>
+      <span class="icons">
+        <i class="fa fa-flag" aria-hidden="true"></i> &nbsp;
+        <i class="fa fa-retweet" aria-hidden="true"></i> &nbsp;
+        <i class="fa fa-heart" aria-hidden="true"></i> &nbsp;
+      </span>
+    </footer>
+
   </article>
 `);
 
-return $tweet;
+  return $tweet;
 }
 
 
