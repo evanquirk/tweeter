fdb06abc924a62ebe8f9f864038255737eb95792
Evan Quirk <evan_quirk@me.com>
2021-02-24 16:12:01 -0800

    chore: remove hard code for user data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,27 +1,27 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const handleTweetSubmit = function() {
 
@@ -49,17 +49,15 @@ function loadTweets () {
   });
 }
 
-
-
 // loops through tweets (below)
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
 const renderTweets = function(tweets) {
   tweets.forEach(function(element) {
     const $tweet = createTweetElement(element);
-    $("#tweet-container").append($tweet);
+    $("#tweet-container").prepend($tweet);
   });
-
+  
 }
 
 const createTweetElement = function(tweet) {
@@ -91,6 +89,6 @@ return $tweet;
 
 
 $(document).ready(function () {
-  renderTweets(data);
+  loadTweets();
   handleTweetSubmit();
 })
